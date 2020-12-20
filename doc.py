3994075c2ee1f19524477de6c8559ea24d15efaf
platon <platonpechenev@yandex.ru>
2020-12-20 19:17:02 +0400

    Добавление записи врачом

--- doc.py
+++ doc.py
@@ -1,5 +1,6 @@
 import datetime as dt
-from PyQt5.QtWidgets import QDialog, QDesktopWidget, QTableWidget, QTableWidgetItem
+from PyQt5.QtWidgets import QDialog, QDesktopWidget, QTableWidget, QTableWidgetItem, QLabel, QComboBox, QPlainTextEdit, \
+    QPushButton
 from PyQt5.QtGui import QColor
 
 from database import DataBase
@@ -26,8 +27,17 @@ class DocWidget(QDialog):
         self.setTableDates()
         self.table.setStyleSheet('gridline-color: #6B6B6B')
         self.table.cellClicked.connect(self.info)
-        self.table.resizeColumnsToContents()
+        self.table.cellDoubleClicked.connect(self.new_appoint)
         # ЗАМЕНИТЬ ПОТОМ НА cellClicked
+
+    def new_appoint(self, r, c):
+        item = self.table.item(r, c)
+        date, time = self.dates[c + 1], self.times[r]
+        if item.text() == ' ' and item.background().color().name() == '#ebebeb':
+            addApp = NewAppoinment(time, date, self.docId)
+            addApp.show()
+            addApp.exec_()
+            self.setTableDates()
 
     def info(self):
         if self.table.currentItem().text() != ' ':
@@ -76,9 +86,11 @@ class DocWidget(QDialog):
         now = dt.date.today()
         lst = []
         con = DataBase()
+        self.dates = [now]
         for i in range(15):
             delta = dt.timedelta(days=i)
             var = now + delta
+            self.dates.append(var)
             wd = var.strftime('%A')
             wd = self.table_trans[wd]
             lst.append(wd + var.strftime(',  %d %b'))
@@ -108,7 +120,56 @@ class DocWidget(QDialog):
         self.table.setHorizontalHeaderLabels(lst)
         for i in range(15):
             self.table.horizontalHeaderItem(i).setBackground(QColor(245, 245, 245))
+        self.table.resizeColumnsToContents()
 
     def color(self, table, i, color):
         for j in range(table.columnCount()):
             table.item(i, j).setBackground(color)
+
+
+class NewAppoinment(QDialog):
+    def __init__(self, time, date, docId):
+        super().__init__()
+        self.resize(400, 360)
+        self.time, self.date, self.docid = time, date, docId
+        day = date.strftime('%d %b')
+
+        date_label = QLabel(self)
+        date_label.setText(f'Дата:\t\t{day}')
+        date_label.move(15, 10)
+
+        time_label = QLabel(self)
+        time_label.setText(f'Время:\t\t{time}')
+        time_label.move(15, 40)
+
+        patient_label = QLabel(self)
+        patient_label.setText(f'Пациент:\t')
+        patient_label.move(15, 70)
+
+        self.con = DataBase()
+
+        self.patients_combo = QComboBox(self)
+        self.patients_combo.move(115, 65)
+        patients = self.con.get_data('patients', 'surname, name, patronymic')
+        for i in range(len(patients)):
+            surname, name, patronymic = patients[i]
+            self.patients_combo.addItem(f'{surname} {name} {patronymic}')
+
+        appointments_label = QLabel(self)
+        appointments_label.setText('Жалобы:\t\t')
+        appointments_label.move(15, 100)
+
+        self.appointments_input = QPlainTextEdit(self)
+        self.appointments_input.setGeometry(125, 100, 250, 190)
+
+        self.add_appointment_btn = QPushButton(self)
+        self.add_appointment_btn.move(280, 305)
+        self.add_appointment_btn.setText('Создать')
+        self.add_appointment_btn.clicked.connect(self.new_appointment)
+
+    def new_appointment(self):
+        day = self.date.strftime('%d %b')
+        sql = [self.docid, self.patients_combo.currentIndex() + 1, self.appointments_input.toPlainText(), self.time,
+               day]
+        self.con.add_data('appointments', sql)
+        self.hide()
